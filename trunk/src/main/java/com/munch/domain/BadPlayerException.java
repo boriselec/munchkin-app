44056b8fd9bced3fcbdc2473b37417bc60e3e5d9
boris.puzikov@gmail.com <boris.puzikov@gmail.com@0f57300c-e986-99e5-6a09-ac113096e6ed>
2013-06-12 21:59:44 +0000

    init

---
+++ trunk/src/main/java/com/munch/domain/BadPlayerException.java
@@ -0,0 +1,12 @@
+package com.munch.domain;
+
+/**
+ * User: boris
+ * Date: 08.06.13
+ * Time: 22:00
+ */
+public class BadPlayerException extends Exception {
+    public BadPlayerException(String s) {
+        super(s);
+    }
+}
